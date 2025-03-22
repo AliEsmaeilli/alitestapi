20ee180d69549ddf16c7220a9d5b2c16623d4589
AliEsmaeili32 <ali.esmaeili@greenstuffworld.com>
2025-03-22 22:15:45 +0330

    Fix cors

--- myaz-aci-workshop/Program.cs
+++ myaz-aci-workshop/Program.cs
@@ -2,23 +2,24 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin() // Allow all origins
+              .AllowAnyHeader() // Allow any headers
+              .AllowAnyMethod(); // Allow any method (GET, POST, etc.)
+    });
+});
+
+// Add controllers
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-//if (app.Environment.IsDevelopment())
-//{
-app.UseSwagger();
-app.UseSwaggerUI();
-//}
-Console.WriteLine("now using latest version");
+// Use CORS
+app.UseCors("AllowAll");  // Apply the CORS policy
 
 app.UseAuthorization();
-app.UseCors("AllowAll");
 app.MapControllers();
-
 app.Run();
